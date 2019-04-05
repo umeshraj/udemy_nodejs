2fefa0602b0ff7d7ccb776d1ac281fb86fe5ac16
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2019-04-05 06:22:34 -0400

    Nodemon updates without needing restart

--- 3_11_routing_nodemon/app.js
+++ 3_11_routing_nodemon/app.js
@@ -1,6 +1,17 @@
 const express = require("express");
 
 const app = express();
+
+// Index route
+app.get("/", (req, res) => {
+  res.send("INDEX");
+});
+
+// About page
+app.get("/about", (req, res) => {
+  res.send("ABOUT2");
+});
+
 const port = 5000;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
