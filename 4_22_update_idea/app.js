03668bfbdd897199316892796891f5b9f45f0d8e
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2019-04-08 06:38:58 -0400

    Updating an existing idea

--- 4_22_update_idea/app.js
+++ 4_22_update_idea/app.js
@@ -112,7 +112,18 @@ app.post("/ideas", (req, res) => {
 
 // Edit form process
 app.put("/ideas/:id", (req, res) => {
-  res.send("PUT");
+  // res.send("PUT");
+  Idea.findOne({
+    _id: req.params.id
+  }).then(idea => {
+    // new values
+    idea.title = req.body.title;
+    idea.details = req.body.details;
+
+    idea.save().then(idea => {
+      res.redirect("/ideas");
+    });
+  });
 });
 
 const port = 5000;
