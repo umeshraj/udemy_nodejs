06d615fa2edefc3895040c13300216c4b9c28632
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2019-04-10 07:32:32 -0400

    Avoid duplicate users

--- 4_29_user_registration/routes/users.js
+++ 4_29_user_registration/routes/users.js
@@ -40,29 +40,39 @@ router.post("/register", (req, res) => {
       password2: req.body.password2
     });
   } else {
-    // res.send("passed");
-    const newUser = new User({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password
-    });
-    bcrypt.genSalt(10, (err, salt) => {
-      bcrypt.hash(newUser.password, salt, (err, hash) => {
-        if (err) throw err;
-        newUser.password = hash;
-        newUser
-          .save()
-          .then(user => {
-            req.flash("success_msg", "You are now registered and can log in");
-            res.redirect("/users/login");
-          })
-          .catch(err => {
-            console.log(err);
-            return;
+    User.findOne({ email: req.body.email }).then(user => {
+      if (user) {
+        req.flash("error_msg", "Email already registered");
+        res.redirect("/users/register");
+      } else {
+        const newUser = new User({
+          name: req.body.name,
+          email: req.body.email,
+          password: req.body.password
+        });
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser
+              .save()
+              .then(user => {
+                req.flash(
+                  "success_msg",
+                  "You are now registered and can log in"
+                );
+                res.redirect("/users/login");
+              })
+              .catch(err => {
+                console.log(err);
+                return;
+              });
           });
-      });
+        });
+        console.log(newUser);
+      }
     });
-    console.log(newUser);
+    // res.send("passed");
   }
 });
 
