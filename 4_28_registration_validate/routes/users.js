a7424f06c34a79a34d3f8b3cb3f75dcc233a715e
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2019-04-10 06:45:57 -0400

    Check for errors in passwords

--- 4_28_registration_validate/routes/users.js
+++ 4_28_registration_validate/routes/users.js
@@ -14,8 +14,28 @@ router.get("/register", (req, res) => {
 
 // register form POST
 router.post("/register", (req, res) => {
-  console.log(req.body);
-  res.send("Register");
+  // console.log(req.body);
+  // res.send("Register");
+  let errors = [];
+  if (req.body.password != req.body.password2) {
+    errors.push({ text: "Passwords do not match!" });
+  }
+
+  if (req.body.password.length < 2) {
+    errors.push({ text: "Passwords should be atleast 2 characters" });
+  }
+
+  if (errors.length > 0) {
+    res.render("users/register", {
+      errors: errors,
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      password2: req.body.password2
+    });
+  } else {
+    res.send("passed");
+  }
 });
 
 module.exports = router;
