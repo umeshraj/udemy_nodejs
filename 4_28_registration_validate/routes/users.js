e73f34280de7fdf6b22b08a1347d8702c1e4136c
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2019-04-10 06:37:22 -0400

    Handle registration post

--- 4_28_registration_validate/routes/users.js
+++ 4_28_registration_validate/routes/users.js
@@ -2,14 +2,20 @@ const express = require("express");
 const mongoose = require("mongoose");
 const router = express.Router();
 
-// User login
+// User login route
 router.get("/login", (req, res) => {
   res.render("users/login");
 });
 
-// User register
+// User register route
 router.get("/register", (req, res) => {
   res.render("users/register");
+});
+
+// register form POST
+router.post("/register", (req, res) => {
+  console.log(req.body);
+  res.send("Register");
 });
 
 module.exports = router;
