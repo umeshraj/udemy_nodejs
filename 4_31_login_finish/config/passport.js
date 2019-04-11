e64dd7780e27f30056cb769e2b70add2dc36a6cc
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2019-04-11 07:31:05 -0400

    Checking user and password

--- 4_31_login_finish/config/passport.js
+++ 4_31_login_finish/config/passport.js
@@ -7,8 +7,32 @@ const User = mongoose.model("users");
 
 module.exports = function(passport) {
   passport.use(
-    new LocalStrategy({ usernameField: "email" }, (email, passport, done) => {
-      console.log(email);
+    new LocalStrategy({ usernameField: "email" }, (email, password, done) => {
+      // console.log(email);
+      // Match user
+      User.findOne({
+        email: email
+      })
+        .then(user => {
+          if (!user) {
+            return done(null, false, { message: "No User Found" });
+          }
+
+          // console.log(password);
+          // Match password
+          bcrypt.compare(password, user.password, (err, isMatch) => {
+            console.log(password);
+            if (err) throw err;
+            if (isMatch) {
+              return done(null, user);
+            } else {
+              return done(null, false, { message: "Password incorrect" });
+            }
+          });
+        })
+        .catch(err => {
+          console.log("Error!!!!!");
+        });
     })
   );
 };
