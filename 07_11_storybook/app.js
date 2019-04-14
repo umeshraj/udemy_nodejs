b84f591090eaea0af8b215223fc978d367073194
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2019-04-14 16:52:32 -0400

    Setting up google oauth

--- 07_11_storybook/app.js
+++ 07_11_storybook/app.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const passport = require("passport");
+
+// passport config
+require("./config/passport")(passport);
 
 // Load routes
 const auth = require("./routes/auth");
--- 07_11_storybook/routes/auth.js
+++ 07_11_storybook/routes/auth.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const passport = require("passport");
 
-router.get("/google", (req, res) => {
-  res.send("auth");
-});
+router.get(
+  "/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"]
+  })
+);
 
 module.exports = router;
