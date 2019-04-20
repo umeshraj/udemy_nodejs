4eb30a5dba93aff42235307f3610b938ae10bd1d
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2019-04-20 08:34:53 -0400

    Save story to mongodb

--- 07_11_storybook/routes/stories.js
+++ 07_11_storybook/routes/stories.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const Story = mongoose.model("stories");
+const User = mongoose.model("users");
+const Sample = mongoose.model("samples");
 const { ensureAuthenticated, ensureGuest } = require("../helpers/auth");
 
 // Stories index
@@ -10,6 +14,30 @@ router.get("/", (req, res) => {
 // Add story form
 router.get("/add", ensureAuthenticated, (req, res) => {
   res.render("stories/add");
+});
+
+// process add story
+router.post("/", (req, res) => {
+  let allowComments;
+
+  if (req.body.allowComments) {
+    allowComments = true;
+  } else {
+    allowComments = false;
+  }
+
+  const newStory = {
+    title: req.body.title,
+    body: req.body.body,
+    status: req.body.status,
+    allowComments: allowComments,
+    user: req.user.id
+  };
+
+  // create story
+  new Story(newStory).save().then(story => {
+    res.redirect(`/stories/show/${story.id}`);
+  });
 });
 
 module.exports = router;
