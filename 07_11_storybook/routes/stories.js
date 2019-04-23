3fd86c8f21102681dfade9243196fade2836a646
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2019-04-23 06:13:23 -0400

    Show stories from a particular user

--- 07_11_storybook/routes/stories.js
+++ 07_11_storybook/routes/stories.js
@@ -31,6 +31,17 @@ router.get("/show/:id", (req, res) => {
     });
 });
 
+// List stories from a user
+router.get("/user/:userId", (req, res) => {
+  Story.find({ user: req.params.userId, status: "public" })
+    .populate("user")
+    .then(stories => {
+      res.render("stories/index", {
+        stories: stories
+      });
+    });
+});
+
 // Add story form
 router.get("/add", ensureAuthenticated, (req, res) => {
   res.render("stories/add");
